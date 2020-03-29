e7c393661791c6fde933cd38f8d175c02f434cc7
drilon.halili <drilon.halili@gmail.com>
2020-03-29 12:13:14 +0200

    Updates

---
+++ precache-manifest.70fe7087a1d0cccac74c9d8d1b81dd60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d6590cb72a6e65cf12c02e12ee04be",
+    "url": "/drilonhalili/contactmanager/index.html"
+  },
+  {
+    "revision": "ca985b05f325ce666b1c",
+    "url": "/drilonhalili/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ca985b05f325ce666b1c",
+    "url": "/drilonhalili/contactmanager/static/js/2.c7e6bbdd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/drilonhalili/contactmanager/static/js/2.c7e6bbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9163ab17ac80d8243bc8",
+    "url": "/drilonhalili/contactmanager/static/js/main.57bb7678.chunk.js"
+  },
+  {
+    "revision": "938ccb01df21930a4b74",
+    "url": "/drilonhalili/contactmanager/static/js/runtime-main.7783a6a6.js"
+  }
+]);
